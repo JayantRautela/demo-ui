1d12292032e9cf0dc79c85d58a781ff9b26eee98
JayantRautela <jayantrautela0205@gmail.com>
2025-09-16 01:11:40 +0530

    fix some styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar () {
         <div className="w-full">
             <nav className="w-full flex items-center justify-between px-12 py-6 shadow-lg">
                 <p className="text-3xl font-bold"><Link href={"/"}>Logo</Link></p>
-                <div className="flex items-center gap-3 text-gray-600">
+                <div className="flex items-center gap-5 text-gray-600">
                     <Link href={"/features"} className="hover:text-gray-950">Features</Link>
                     <Link href={"/why-choose-us"} className="hover:text-gray-950">Why Choose Us</Link>
                     <Link href={"/how-it-works"} className="hover:text-gray-950">How it Works</Link>
--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -2,7 +2,7 @@
 
 export default function Partner () {
     return (
-        <div className="w-full flex items-center justify-between px-16 mb-16">
+        <div className="w-full flex items-center justify-between px-16 mb-20 mt-8">
             <p className="font-semibold">WE ARE PARTNERED WITH MORE THAN 50+ <br /> COMPANIES AROUND THE GLOBE</p>
             <div className="flex items-center gap-16 font-bold">
                 <p>Some Logo</p>
