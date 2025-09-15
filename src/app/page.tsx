ab957f6805e4e51003a7b17a0c921c0aa49d1b66
JayantRautela <jayantrautela0205@gmail.com>
2025-09-15 20:05:33 +0530

    add trusted partner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import Partner from "@/components/Partner";
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       {/* More components to add */}
       <Hero />
+      <Partner />
     </div>
   );
 }
---
+++ src/components/Partner.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Partner () {
+    return (
+        <div className="w-full flex items-center justify-between px-16 mb-16">
+            <p className="font-semibold">WE ARE PARTNERED WITH MORE THAN 50+ <br /> COMPANIES AROUND THE GLOBE</p>
+            <div className="flex items-center gap-16 font-bold">
+                <p>Some Logo</p>
+                <p>Some Logo</p>
+                <p>Some Logo</p>
+                <p>Some Logo</p>
+                <p>Some Logo</p>
+                <p>Some Logo</p>
+            </div>
+        </div>
+    )
+}
